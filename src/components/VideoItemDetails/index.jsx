75929c619e28726297b0cbd9fe42037f2c00c6e0
lokeshsai-l7 <lokeshsai.polishetty@gmail.com>
2024-12-27 13:04:14 +0530

    Your commit message

--- src/components/VideoItemDetails/index.jsx
+++ src/components/VideoItemDetails/index.jsx
@@ -1,11 +1,20 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { themeState, savedVideosAtom } from '../../recoil_state';
+import { useRecoilValue, useRecoilState } from 'recoil';
 import { themeState } from '../../recoil_state';
 import { useRecoilValue } from 'recoil';
 import Cookies from 'js-cookie';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import ReactPlayer from 'react-player';
+import { formatDistanceToNow } from 'date-fns';
+import { GoDotFill } from 'react-icons/go';
+import { BiSolidLike } from 'react-icons/bi';
+import { BiLike } from 'react-icons/bi';
+import { BiDislike } from 'react-icons/bi';
+import { BiSolidDislike } from 'react-icons/bi';
+import { MdPlaylistAdd } from 'react-icons/md';
 
 const appConstants = {
   initial: 'INITIAL',
@@ -17,6 +26,31 @@ const appConstants = {
 const VideoItemDetails = () => {
   const [videoItemDetailsData, setVideoItemDetailsData] = useState({});
   const [appStatus, setAppStatus] = useState(appConstants.inProgress);
+  const [status, setStatus] = useState(null);
+  const [saved, setSaved] = useState(false); // 'like', 'dislike', or null
+  const [savedVideos, setSavedVideos] = useRecoilState(savedVideosAtom);
+  const darkTheme = useRecoilValue(themeState);
+  const { id } = useParams();
+  const { videoUrl, title, publishedAt, viewCount } = videoItemDetailsData;
+  console.log(savedVideos);
+  const handleLike = () => {
+    setStatus((prev) => (prev === 'like' ? null : 'like')); // Toggle like
+  };
+
+  const handleDislike = () => {
+    setStatus((prev) => (prev === 'dislike' ? null : 'dislike')); // Toggle dislike
+  };
+
+  const savingVideos = () => {
+    if (saved === true) {
+      setSavedVideos([...savedVideos, videoItemDetailsData]);
+    }
+  };
+
+  const handleSave = () => {
+    setSaved(!saved, savingVideos());
+  };
+
   const darkTheme = useRecoilValue(themeState);
   const { id } = useParams();
   const { videoUrl } = videoItemDetailsData;
@@ -44,13 +78,16 @@ const VideoItemDetails = () => {
         },
         description: videoDetails.description,
         id: videoDetails.id,
+        publishedAt: formatDistanceToNow(new Date(videoDetails.published_at)),
         publishedAt: videoDetails.published_at,
         thumbnailUrl: videoDetails.thumbnail_url,
         title: videoDetails.title,
         videoUrl: videoDetails.video_url,
         viewCount: videoDetails.view_count,
       };
+
       console.log(updatedData);
+
       setVideoItemDetailsData(updatedData);
     } catch (error) {
       console.log(error);
@@ -59,6 +96,35 @@ const VideoItemDetails = () => {
 
   return (
     <PlayerContainer $darkTheme={darkTheme}>
+      <VideoPlayerContainer>
+        <ReactPlayer
+          url={videoUrl}
+          controls={true}
+          width="100%"
+          height="100%"
+        />
+      </VideoPlayerContainer>
+      <Title>{title}</Title>
+      <NumberContainer>
+        <Views>{viewCount} Views</Views>
+        <DotContainer>
+          <GoDotFill />
+        </DotContainer>
+        <Views>{publishedAt}</Views>
+      </NumberContainer>
+      <NumberContainer>
+        <ButtonItem onClick={handleLike} $liked={status === 'like'}>
+          {status === 'like' ? <BiSolidLike /> : <BiLike />}
+          {`Like`}
+        </ButtonItem>
+        <ButtonItem onClick={handleDislike} $liked={status === 'dislike'}>
+          {status === 'dislike' ? <BiSolidDislike /> : <BiDislike />}
+          {`Dislike`}
+        </ButtonItem>
+        <ButtonItem onClick={handleSave} $liked={saved}>
+          <MdPlaylistAdd />
+        </ButtonItem>
+      </NumberContainer>
       <ReactPlayer url={videoUrl} controls={true} width="100%" height="50%" />
     </PlayerContainer>
   );
@@ -78,4 +144,46 @@ export const PlayerContainer = styled.div`
   &::-webkit-scrollbar {
     display: none;
   }
+`;
+
+export const VideoPlayerContainer = styled.div`
+  height: 40%;
+  @media screen and (min-width: 768px) {
+    height: 70%;
+  }
+`;
+
+export const Title = styled.h1`
+  font-family: 'Roboto';
+  color: #383838;
+  font-size: 20px;
+`;
+
+export const NumberContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Views = styled.p`
+  color: #606060;
+  font-family: 'Roboto';
+  font-size: 16px;
+  margin-right: 10px;
+`;
+
+const DotContainer = styled.div`
+  margin-right: 10px;
+  font-size: 10px;
+`;
+
+const ButtonItem = styled.button`
+  color: ${(props) => (props.$liked ? '#3b82f6' : '#606060')};
+  background-color: transparent;
+  font-size: 18px;
+  border: none;
+  outline: none;
+  display: flex;
+  align-items: center;
+  margin-right: 20px;
+  font-family: 'Roboto';
 `;
